467e7232087bd9d39e3cf2f075be3f34c68467ab
Karpaga Ganesh Patchirajan <karpagaganesh_patchirajan@intuit.com>
2018-05-20 13:43:49 -0500

    init commit

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,8 +1,19 @@
-rfSiteCheckApp.controller("siteController", ['$scope','siteService',function($scope, siteService){
+rfSiteCheckApp.controller("siteController",
+    ['$scope', 'siteService', 'NgTableParams' ,
+        function($scope, siteService, NgTableParams){
     var calcExtractionModel = $scope;
-    console.log('FFF')
+    calcExtractionModel.tabContentUrl = "views/list_sites.html"
     calcExtractionModel.forms = siteService.getFormName();
     calcExtractionModel.selectedForm = siteService.getSelectedForm();
+    calcExtractionModel.items = [
+        {Product:"Moto G2", Desc: "Android Phone", Price: 10999},
+        {Product:"The Monk who sold his ferrari", Desc: "Motivational book", Price: 250},
+        {Product:"Mi Power Bank", Desc: "Power Bank", Price: 999},
+        {Product:"Dell Inspiron 3537", Desc: "Laptop", Price: 45600}
+    ];
+
+    var data = [{siteId: 'AXL12345', operator: 'ATT', state: 'Texas'}];
+    calcExtractionModel.tableParams = new NgTableParams({}, { dataset: data});
 
     calcExtractionModel.selectedOption = $scope.options;
     calcExtractionModel.getFormSelectedForm = function(formName){
