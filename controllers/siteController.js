192a09fea88d6a63dd6b3a2e66a454d0580ddba4
Karpaga Ganesh Patchirajan <karpagaganesh_patchirajan@intuit.com>
2018-05-20 14:03:13 -0500

    init commit

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -2,11 +2,12 @@ rfSiteCheckApp.controller("siteController",
     ['$scope', 'siteService', 'NgTableParams' ,
         function($scope, siteService, NgTableParams){
     var siteControllerModel = $scope;
-            siteControllerModel.tabContentUrl = siteService.getTabUrlValues()[0];
+    siteControllerModel.tabContentUrl = siteService.getTabUrlValues()[0];
 
     var data = [{siteId: 'AXL12345', operator: 'ATT', state: 'Texas'}];
     siteControllerModel.tableParams = new NgTableParams({}, { dataset: data});
 
+    siteControllerModel.sites = [{name: 'All Sites', page:'list_sites'}]
 
     siteControllerModel.setTabContent = function(name) {
         siteControllerModel.tabContentUrl = "views/" +name+".html";
